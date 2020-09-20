13c1460d4b23ec1a615ba67d62d50b2fbb5cd78a
Zain Raja <zraja1105@gmail.com>
2020-09-20 20:13:04 +0100

    Message box is cleared once a message is sent

--- macro.vb
+++ macro.vb
@@ -1,17 +1,29 @@
 Sub sendMessage()
+'   Change these values to change where the
+'   Information is taken from
+
+    Dim nameCell, messageCell As String
+    nameCell = "B3"
+    messageCell = "E7"
+    
     Dim url As String
     Dim name, message As String
     
     url = "http://7seven77.000webhostapp.com/send.php"
-
-    name = Range("B3").Value
-    message = Range("E7").Value
     
+'   Obtain the cell values
+    name = Range(nameCell).Value
+    message = Range(messageCell).Value
+    
+'   Create the full url
     url = url + "?name=" + name + "&message=" + message
-    
+
+'   Make the request to send a message
     result = request(url)
-    
     Debug.Print result
+
+'   Reset the message cell ready for another message
+    Range(messageCell).Value = ""
 End Sub
 
 Function request(url As String) As String
