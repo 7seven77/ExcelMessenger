561b17b4a41017a45f38e6977d32dbc929f07ffd
Zain Raja <zraja1105@gmail.com>
2020-09-24 12:33:04 +0100

    Fix bug in cell retrieval functions

--- data.vb
+++ data.vb
@@ -1,36 +1,36 @@
-' Functions that return cells / values that are specified by you
-
 ' Should match the maximum number of messages being used in your db
 Function getMaximumNumberOfMessages() As Integer
     getMaximumNumberOfMessages = 10
 End Function
+
+' Functions to get needed data from the spreadsheet
 
 ' Change these functions so they return the cells
 ' Where the data is located
 
 Function getSenderCell() As String
 '   This should be the cell where the sender name is
-    getSenderCelll = "B3"
+    getSenderCell = "B3"
 End Function
 
 Function getRecipientCell() As String
 '   This should be the cell where the recipient name is
-    getRecipientCell = "B5"
+    getRecipientCell = "B8"
 End Function
 
 Function getMessageCell() As String
 '   This should be the cell where the message is
-    getMessageCell = "E7"
+    getMessageCell = "G8"
 End Function
 
 Function getMessagesColumn() As String
 '   The column which the messages should be displayed in
-    getMessagesColumn = "E"
+    getMessagesColumn = "G"
 End Function
 
 Function getMessagesStartRow() As Integer
 '   The row which the messages should start being displayed on
-    getMessagesStartRow = 13
+    getMessagesStartRow = 14
 End Function
 
 ' Get the value from a cell
@@ -39,11 +39,11 @@ Function getCell(position As String) As String
 End Function
 
 Function getSender() As String
-    getSender = getCell("B3")
+    getSender = getCell(getSenderCell())
 End Function
 
 Function getRecipient() As String
-    getRecipient = getCell("B5")
+    getRecipient = getCell(getRecipientCell())
 End Function
 
 Function getMessage() As String
