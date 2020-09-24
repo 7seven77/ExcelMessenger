b4fa823f88f203f1b15ac2ad2e4ce218a4972bd2
Zain Raja <zraja1105@gmail.com>
2020-09-24 18:19:36 +0100

    Add timer for status message

--- status.vb
+++ status.vb
@@ -4,6 +4,28 @@ Function getStatusCell() As String
     getStatusCell = "G3"
 End Function
 
+Function getStatus() As String
+    getStatus = getCell(getStatusCell())
+End Function
+
+Sub setStatus(newValue As String)
+    Range(getStatusCell()).value = newValue
+End Sub
+
 Sub updateStatus(newValue As String)
-    Range(getStatusCell()).Value = newValue
+    Call setStatus(newValue)
+    
+    Dim original As String
+    original = getStatus()
+    
+    Dim WaitTime As Date
+    WaitTime = Now() + TimeValue("00:00:05")
+    While Now() < WaitTime
+        DoEvents
+    Wend
+    
+    If getStatus() = original Then
+        Call setStatus("")
+    End If
+    
 End Sub
